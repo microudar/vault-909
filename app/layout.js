a297ae88618b530c28d6c3e0336348d54c4dec7a
microudar <micro_udar12@mail.ru>
2026-04-22 21:20:21 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Архив 909',
+  description: 'Музыкальный архив',
+}
+
+import './globals.css'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  )
+}
