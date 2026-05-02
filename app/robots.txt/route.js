20c3c0b31d2ffd7ff95a760bda0d5ccde22b2433
microudar <micro_udar12@mail.ru>
2026-05-02 15:53:18 +0700

    Update route.js

--- app/robots.txt/route.js
+++ app/robots.txt/route.js
@@ -1,1 +1,11 @@
+export function GET() {
+  return new Response(
+`User-agent: *
+Allow: /
 
+Sitemap: https://vault909.ru/sitemap.xml`,
+    {
+      headers: { 'Content-Type': 'text/plain' },
+    }
+  )
+}
